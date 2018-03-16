b1acd2e0ff0976b256244135b70f82348c5fae9d
James Morris <blondfro@gmail.com>
2018-03-16 12:47:58 -0500

    added in http functionality to get and post data to a server.  Now pulling the list of languages from a server rather than locally.  Set up observables on the services.
     removed the list of languages from being hard-coded in the home component. added in the ability to subscribe to the observables from the service.
    
     added in an if statement to only load the page once the languages from the server had ben retrieved.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,12 +9,16 @@ import {NgForm} from "@angular/forms";
   templateUrl: './home.component.html'
 })
 export class HomeComponent {
-  languages = ['English', 'Spanish', 'French', 'Other'];
+  languages = [];
   model = new Employee('Darla', 'Smith', true, 'w2', 'default');
   hasPrimaryLanguageError = false;
 
   constructor (private formPoster: FormPoster) {
-
+    this.formPoster.getLanguages()
+      .subscribe(
+        data => this.languages = data.languages,
+        err => console.log('get error: ', err)
+      );
   }
 
   submitForm(form: NgForm) {
@@ -23,7 +27,11 @@ export class HomeComponent {
     if (this.hasPrimaryLanguageError)
       return;
 
-    this.formPoster.postEmployeeForm(this.model);
+    this.formPoster.postEmployeeForm(this.model)
+      .subscribe(
+        data => console.log('success: ', data),
+        err => console.log('error: ', err)
+      )
   }
 
   validatePrimaryLanguage(value) {
